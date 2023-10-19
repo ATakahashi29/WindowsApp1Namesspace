cced6ceb5256ef8888b752655195ee5e80f4fb91
arinapc <a-takahashi@gaialink.co.jp>
2023-10-19 15:21:55 +0900

    import宣言の別名命名追加

--- WindowsApp1Namesspace/Form1.vb
+++ WindowsApp1Namesspace/Form1.vb
@@ -1,9 +1,15 @@
-﻿Imports WindowsApp1Namesspace.Akita
-Imports WindowsApp1Namesspace.Iwate
+﻿'imports宣言する時に、別名命名
+Imports Nihonkai = WindowsApp1Namesspace.Akita
+Imports Taiheiyo = WindowsApp1Namesspace.Iwate
 
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim Space As New Space()
+        '↓このままではダブっていて名前空間追記必要
+        'Dim Space As New Space()
+
+        'どちらか必須↓
+        Dim Space As New Nihonkai.Space()
+        'Dim Space As New Taiheiyo.Space()
         Space.ShowName()
     End Sub
 End Class
